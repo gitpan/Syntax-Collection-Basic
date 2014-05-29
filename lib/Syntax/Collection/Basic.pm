ed4bf5ea679b5fae2ba2f096669226b5c90ca377
Erik Carlsson <info@code301.com>
2014-05-29 19:37:38 +0000

    Import of CSSON/Syntax-Collection-Basic-0.01 from CPAN.
    gitpan-cpan-distribution: Syntax-Collection-Basic
    gitpan-cpan-version:      0.01
    gitpan-cpan-path:         CSSON/Syntax-Collection-Basic-0.01.tar.gz
    gitpan-cpan-author:       CSSON
    gitpan-cpan-maturity:     released

---
+++ lib/Syntax/Collection/Basic.pm
@@ -0,0 +1,25 @@
+package Syntax::Collection::Basic;
+
+use 5.010;
+
+our $VERSION = "0.01";
+
+
+use Syntax::Collector q/
+	use strict 0;
+	use warnings 0;
+	use Modern::Perl 0 '2014';
+	use true 0;
+/;
+
+1;
+
+# ABSTRACT: Standard boilerplate
+
+=head1 AUTHOR
+
+Erik Carlsson
+
+=head1 LICENSE
+
+Artistic license
